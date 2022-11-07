d1f302645151ba2e38d9163c73b026b29e75789e
Dylan Kirdahy <me@dylankirdahy.nyc>
2022-11-06 19:39:18 -0500

    combined programs

--- mu.py
+++ mu.py
@@ -8,4 +8,9 @@ Tn = T/300
 
 mu_e = 88*Tn**(-0.57) + (7.4e8*T**(-2.33)) / (1 + (N*0.88*Tn**(-0.164))/(1.26e17*Tn**2.4))
 
-print(mu_e)
+mu_h = 54.3*(Tn**(-0.57)) + (2.406e8*(T**(-2.33))) / (1 + (N*0.88*(Tn**(-0.164)))/(2.35e17*(Tn**2.4)))
+
+#Originally 2.406 was 1.36 but that didn't seem to work properly
+
+print("mu sub e:",mu_e)
+print("mu sub h:",mu_h)
